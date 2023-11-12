022a2eae4076045b53f9b9511b851c862e40d228
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2023-11-12 22:45:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/balaji-portfolio/',
   build: {
-    outDir: 'dist',  // Output directory for built files
+    outDir: 'public',  // Output directory for built files
   },
   publicDir: 'public',
 })
