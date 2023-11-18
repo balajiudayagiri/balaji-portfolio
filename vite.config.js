bb0cb159cdf19a8ac77ff89ba6e7fc188125ec25
balaji-pcs <131011674+balaji-pcs@users.noreply.github.com>
2023-11-19 01:41:48 +0530

    UPD routes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import reactRefresh from "@vitejs/plugin-react-refresh";
-import reactJsx from "vite-react-jsx";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import ViteSSR from 'vite-plugin-ssr/plugin';
 
-// Set the base path to your GitHub repository name
 export default defineConfig({
-  base: "/balaji-portfolio/", // replace with your repository name
-  plugins: [react(), reactRefresh(), reactJsx(), ViteSSR()],
+  plugins: [react(), ViteSSR()],
   build: {
-    outDir: "dist", // Output directory for built files
+    outDir: 'dist',
   },
-  publicDir: "public",
+  publicDir: 'public',
 });
